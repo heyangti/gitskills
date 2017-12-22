102bb6d19bd9d9b1dc4a3ae8e6e82811ac7789e1
heyangti <heyangti@yy.com>
2017-12-22 10:08:50 +0800

    添加一个unity工程

---
+++ AirplainPro/Assets/scripts/test/collider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collider : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name + "撞过来了");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("撞着");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("离开");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+    }
+}
